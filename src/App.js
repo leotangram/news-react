9948a9838661411c75b63fac4e492a78be8c8db7
Leonardo Omaña <leotangram@gmail.com>
2019-06-01 10:38:44 -0500

    Check api news.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    news: []
+  }
+  componentDidMount() {
+    this.checkNews()
+  }
+
+  checkNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=5f57f9847d7f4b3ebdc9f00a582b78c6`
+    const result = await fetch(url)
+    const news = await result.json()
+    this.setState({
+      news: news.articles
+    })
+  }
+
   render() {
     return (
       <div>
