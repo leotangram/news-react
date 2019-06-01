112c8f871ad047c590d5d367c5099057ea991e15
Leonardo Omaña <leotangram@gmail.com>
2019-06-01 11:01:47 -0500

    Show info news.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from 'react'
 
-const Article = () => {
+const Article = ({ article }) => {
+  // Extraer los datos
+  const { urlToImage, url, title, description, source } = article
   return (
-    <h1>Desde Article</h1>
-  );
+    <div className='col s12 m6 l4'>
+      <div className='card'>
+        <div className='card-image'>
+          <img src={urlToImage} alt={title} />
+        </div>
+        <div className='card-content'>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className='card-action'>
+          <a
+            href={url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='waves-effect waves-light btn'
+          >
+            Ver noticia completa
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
- 
-export default Article;
+
+export default Article
